89d684cd4051b29f9c17bee5191e16605c124ae1
r4moss <davehowardlittleramz@gmail.com>
2023-04-12 21:51:14 +1200

    init

---
+++ script.js
@@ -0,0 +1,17 @@
+let menu= document.querySelector('#menu-icon');
+let navlist= document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+};
+const sr = ScrollReveal ({
+    distance: '65px',
+    duration: 2600,
+    delay: 450,
+    reset: true,
+});
+
+sr.Reveal('.hero-text',{delay:200, origin: 'top'});
+sr.Reveal('.hero-img',{delay:450, origin: 'top'});
+sr.reveal('.icons',{delay:500, origin: 'left'});
